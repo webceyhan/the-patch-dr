e2a5cb29d61301683f10e880c61797dd4d551879
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-07 00:45:40 +0200

    Add Vite project files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import OldPage from "./components/OldPage.vue";
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div> -->
+
+  <OldPage />
+</template>
