1dbd824e58727473581c02a5d6a7f7182c55b9ae
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-07 20:55:01 +0200

    Migrate footer section

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <div class="container text-center">
+      <p>Copyright © The Patch Dr. 2024</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  padding: 25px 0;
+}
+
+footer p {
+  margin: 0;
+}
+</style>
