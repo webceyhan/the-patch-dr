dfc9102c6aeff1fff8d479ac1941548e4e03c10e
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-07 14:29:53 +0200

    Replace fontawesome icons with FaIcon component

---
+++ src/components/FaIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <svg class="icon">
+    <use :xlink:href="`/fa-icons.svg#${name}`" />
+  </svg>
+</template>
+
+<style scoped>
+.icon {
+  width: 16px;
+  height: 16px;
+  fill: currentColor;
+}
+</style>
