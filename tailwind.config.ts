c1fb72c605cb8c5f45ba88494adb691b6534771d
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-08 14:40:38 +0200

    Install and configure daisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
 
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [daisyui],
+    daisyui: {
+        themes: ['light'],
+        darkTheme: 'dark',
+    },
 } satisfies Config;
