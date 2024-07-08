7ec79aa3c9c8f76aad241078d320c93562deb165
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-08 11:08:18 +0200

    Migrate custom fonts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './css/bootstrap.min.css';
 import './css/animate.css';
 import './css/prettyPhoto.css';
 import './css/custom.css';
-import './css/custom-fonts.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
