0b4754275a7fc609b3dbc3f15eb380677a593195
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-08 11:17:19 +0200

    Move custom script into main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+// Smooth Scrolling -----------------------------------------------------------
+const $ = (window as any).$;
+
+$('a.page-scroll').bind('click', function (this: any, event: any) {
+    const scrollTop = $($(this).attr('href')).offset().top;
+
+    $('html, body').stop().animate({ scrollTop }, 1500, 'easeInOutExpo');
+
+    event.preventDefault();
+});
