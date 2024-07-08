595a5439f54a134a742f6774b7f8d31f3feb4a6d
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-08 10:21:10 +0200

    Migrate page scroller

---
+++ src/components/PageScroller.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import FaIcon from "./FaIcon.vue";
+</script>
+
+<template>
+  <div class="scroll-to-top affix" data-spy="affix" data-offset-top="200">
+    <a href="#page-top" class="page-scroll">
+      <FaIcon name="arrow-up" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.scroll-to-top {
+  background: #333;
+  position: fixed;
+  bottom: 15px;
+  right: 15px;
+  text-align: center;
+  border-radius: 4px;
+  z-index: 9999;
+  opacity: 0;
+  filter: alpha(opacity=0);
+  -webkit-transition: opacity 0.5s;
+  transition: opacity 0.5s;
+  opacity: 0.5;
+  filter: alpha(opacity=50);
+}
+.scroll-to-top:hover {
+  opacity: 1;
+  filter: alpha(opacity=100);
+}
+.scroll-to-top a {
+  color: #fff;
+  display: block;
+  line-height: 40px;
+  width: 40px;
+  height: 40px;
+}
+</style>
