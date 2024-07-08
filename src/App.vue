cd323e51c653edfd30267ebaf2f926ae08717094
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-08 10:23:50 +0200

    Move contents from OldPage to App component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <script setup lang="ts">
-import OldPage from "./components/OldPage.vue";
+import ServicesSection from "./components/ServicesSection.vue";
+import FeaturesSection from "./components/FeaturesSection.vue";
+import PortfolioSection from "./components/PortfolioSection.vue";
+import TestimonialsSection from "./components/TestimonialsSection.vue";
+import ContactSection from "./components/ContactSection.vue";
+import FooterSection from "./components/FooterSection.vue";
+import CallActionSection from "./components/CallActionSection.vue";
+import AboutSection from "./components/AboutSection.vue";
+import WelcomeSection from "./components/WelcomeSection.vue";
+import Navbar from "./components/Navbar.vue";
+import Preloader from "./components/Preloader.vue";
+import PageScroller from "./components/PageScroller.vue";
 </script>
 
 <template>
-  <OldPage />
+  <div id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <Preloader />
+
+    <Navbar />
+
+    <WelcomeSection id="welcome" />
+
+    <ServicesSection id="service" />
+
+    <AboutSection id="about" />
+
+    <CallActionSection id="call-action" />
+
+    <FeaturesSection id="features" />
+
+    <PortfolioSection id="portfolio" />
+
+    <TestimonialsSection id="testimonials" />
+
+    <ContactSection id="contact" />
+
+    <FooterSection />
+
+    <PageScroller />
+  </div>
 </template>
