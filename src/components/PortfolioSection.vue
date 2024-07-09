7cf4b2514282051ab8cfbce98785deab91ed3dec
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-10 00:21:40 +0200

    Migrate portfolio section

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { computed, ref } from "vue";
+import FaIcon from "./FaIcon.vue";
 
 const CATEGORIES = [
   "all",
@@ -153,257 +154,89 @@ const ITEMS: Item[] = [
 ];
 
 const filter = ref("all");
+const photos = ref<Item["photos"]>([]);
 
 const filteredItems = computed(() =>
   filter.value === "all"
     ? ITEMS
     : ITEMS.filter((item) => item.categories.includes(filter.value))
 );
-
-onMounted(() => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const $ = (window as any).$ as any;
-
-  $(".portfolio a[data-rel^='prettyPhoto']").prettyPhoto({ hook: "data-rel" });
-});
 </script>
 
 <template>
-  <section class="content-section">
-    <div class="container">
-      <div class="text-center">
-        <h2 class="title light_grey">A few of our recent projects</h2>
-      </div>
+  <section class="container mx-auto p-4 md:py-10 flex flex-col items-center space-y-10">
+    <!-- header -->
+    <header class="text-3xl font-bold uppercase text-center text-neutral-400">
+      A few of our recent projects
+    </header>
 
-      <div class="filters text-center">
-        <button
-          v-for="category in CATEGORIES"
-          :class="['btn btn-default', { active: filter === category }]"
-          @click="filter = category"
-        >
+    <!-- filters -->
+    <ul class="menu menu-sm bg-base-200 rounded-box max-h-14 overflow-x-scroll">
+      <li v-for="category in CATEGORIES">
+        <a :class="{ active: filter === category }" @click.prevent="filter = category">
           {{ category }}
-        </button>
-      </div>
-    </div>
+        </a>
+      </li>
+    </ul>
 
-    <div class="container">
-      <ul class="row portfolio desktop_text">
-        <li
-          v-for="item in filteredItems"
-          class="col-xs-6 col-sm-6 col-lg-3 portfolio-item"
+    <!-- grid -->
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <!-- card -->
+      <div
+        v-for="item in filteredItems"
+        class="card image-full text-base-content shadow-xl overflow-hidden"
+      >
+        <figure>
+          <img
+            :src="`/images/work/${item.id}-cover.jpg`"
+            class="object-cover w-full h-full"
+            :alt="item.title"
+            loading="lazy"
+          />
+        </figure>
+
+        <div
+          class="card-body bg-black/90 opacity-0 hover:opacity-100 transition-all duration-500"
         >
-          <figure>
-            <div>
-              <img
-                :src="`/images/work/${item.id}-cover.jpg`"
-                :alt="item.title"
-                loading="lazy"
-              />
-            </div>
+          <h2 class="card-title uppercase text-primary">{{ item.title }}</h2>
 
-            <figcaption>
-              <h4>{{ item.title }}</h4>
+          <h3 class="text-neutral-400 font-bold">Location: {{ item.location }}</h3>
 
-              <p class="light_grey">Location: {{ item.location }}</p>
+          <p class="text-white py-4">{{ item.description }}</p>
 
-              <p>
-                {{ item.description }}
-              </p>
-
-              <a
-                v-for="photo in item.photos"
-                :href="`/images/work/${photo.name}`"
-                :data-rel="`prettyPhoto[${item.id}]`"
-                :title="photo.alt"
-              >
-                Take a look
-              </a>
-            </figcaption>
-          </figure>
-        </li>
-      </ul>
-
-      <!-- mobile version -->
-      <ul class="row portfolio mobile_text">
-        <li
-          v-for="item in filteredItems"
-          class="col-lg-3 col-sm-6 col-xs-6 portfolio-item"
-        >
-          <figure>
-            <div>
-              <img
-                :src="`/images/work/${item.id}-cover.jpg`"
-                :alt="item.title"
-                loading="lazy"
-              />
-            </div>
-          </figure>
-        </li>
-      </ul>
-
-      <div class="row mobile_text">
-        <div class="col-md-12 text-center">
-          <p class="gallery_note grey">Full Gallery available on Desktop site</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-sm bg-white" @click="photos = item.photos">
+              Take a look
+            </button>
+          </div>
         </div>
       </div>
     </div>
+
+    <Teleport v-if="photos.length" to="body">
+      <div
+        class="fixed inset-0 z-[999] bg-black/90 flex flex-col items-center justify-center p-10"
+      >
+        <button class="btn btn-lg btn-circle fixed top-4 right-4" @click="photos = []">
+          <FaIcon name="x" style="width: 32px; height: 32px" />
+        </button>
+
+        <div class="carousel carousel-center rounded-box w-full">
+          <div v-for="photo in photos" class="carousel-item w-full">
+            <img
+              class="w-full object-cover"
+              :src="`/images/work/${photo.name}`"
+              :alt="photo.alt"
+            />
+          </div>
+        </div>
+      </div>
+    </Teleport>
   </section>
 </template>
 
 <style scoped>
-.content-section {
-  padding-top: 80px;
-  padding-bottom: 50px;
-}
-
-.title {
-  margin-bottom: 35px;
-}
-
-.portfolio {
-  padding: 0;
-  max-width: 1300px;
-  margin: 0 auto 15px;
-  list-style: none;
-}
-
-.portfolio-item {
-  margin: 0;
-  padding: 0;
-  display: inline-block;
-  text-align: left;
-  position: relative;
-}
-.portfolio-item img {
-  width: 100%;
-  height: auto;
-}
-
-.portfolio-item figure {
-  padding: 15px;
-  margin: 0;
-  position: relative;
-  -webkit-transform-style: preserve-3d;
-  -moz-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-}
-.portfolio-item figure > div {
-  overflow: hidden;
-}
-.portfolio-item figure img {
-  max-width: 100%;
-  display: block;
-  position: relative;
-  -webkit-transition: -webkit-transform 0.4s;
-  -moz-transition: -moz-transform 0.4s;
-  transition: transform 0.4s;
-}
-
-.portfolio-item figcaption {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 25px 20px;
-  background: rgba(0, 0, 0, 0.8);
-  color: #000;
-  margin: 15px;
-  height: calc(100% - 30px);
-  width: calc(100% - 30px);
+.card.image-full:before {
   opacity: 0;
-  -webkit-backface-visibility: hidden;
-  -moz-backface-visibility: hidden;
-  backface-visibility: hidden;
-  -webkit-transform-origin: 0 0;
-  -moz-transform-origin: 0 0;
-  transform-origin: 0 0;
-  -webkit-transform: rotateY(-90deg);
-  -moz-transform: rotateY(-90deg);
-  transform: rotateY(-90deg);
-  -webkit-transition: -webkit-transform 0.4s, opacity 0.1s 0.3s;
-  -moz-transition: -moz-transform 0.4s, opacity 0.1s 0.3s;
-  transition: transform 0.4s, opacity 0.1s 0.3s;
-}
-.portfolio-item figcaption h4 {
-  letter-spacing: -1px;
-  margin: 0 0 10px;
-  padding: 0;
-  color: #eb5e54;
-}
-.portfolio-item figcaption p {
-  color: #ffffff;
-  line-height: 1.3;
-}
-.portfolio-item figcaption a {
-  text-align: center;
-  padding: 5px 10px;
-  border-radius: 2px;
-  display: inline-block;
-  background: #fff;
-  color: #5e5f5f;
-  font-weight: 400;
-  position: absolute;
-  bottom: 25px;
-  left: 20px;
-  font-size: 14px;
-  padding: 4px 8px;
-  -webkit-transition: all ease 0.3s;
-  -moz-transition: all ease 0.3s;
-  -ms-transition: all ease 0.3s;
-  -o-transition: all ease 0.3s;
-  transition: all ease 0.3s;
-}
-.portfolio-item figcaption a:hover {
-  color: #ffffff;
-  background: #eb5e54;
-}
-
-.portfolio-item figure:hover img {
-  -webkit-transform: translateX(0%);
-  -moz-transform: translateX(0%);
-  -ms-transform: translateX(0%);
-  transform: translateX(0%);
-}
-.portfolio-item figure:hover figcaption {
-  opacity: 1;
-  -webkit-transform: rotateY(0deg);
-  -moz-transform: rotateY(0deg);
-  transform: rotateY(0deg);
-  -webkit-transition: -webkit-transform 0.4s, opacity 0.1s;
-  -moz-transition: -moz-transform 0.4s, opacity 0.1s;
-  transition: transform 0.4s, opacity 0.1s;
-}
-
-@media (max-width: 436px) {
-  .content-section {
-    padding-top: 60px;
-    padding-bottom: 30px;
-  }
-
-  .portfolio-item figure {
-    padding: 0;
-  }
-  .portfolio-item figcaption {
-    margin: 0;
-    height: 100%;
-    width: 100%;
-  }
-  .portfolio-item figcaption p {
-    display: none;
-  }
-}
-
-@media (min-width: 436px) and (max-width: 800px) {
-  .portfolio-item {
-    width: 50%;
-  }
-}
-
-.pp_gallery,
-div.pp_default .pp_nav .currentTextHolder,
-div.pp_default .pp_expand {
-  display: none !important;
-}
-div.pp_default .pp_description {
-  text-transform: uppercase;
 }
 </style>
