4767f530985803a617653558861f38ceaf93a0bf
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-09 16:36:40 +0200

    Migrate welcome section

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -1,154 +1,34 @@
 <template>
-  <section>
-    <header class="welcome">
-      <div class="welcome-body">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-              <h1 class="brand-heading">
-                SURGERY<span class="friction">,</span> FOR
-                <span class="text-primary">SPACES.</span>
-              </h1>
+  <section
+    class="hero bg-cover bg-center bg-no-repeat text-base-100 h-auto md:min-h-screen py-10 pt-20"
+    style="background-image: url('/images/welcome-section-bg.jpg')"
+  >
+    <div class="hero-content text-center h-full">
+      <div class="max-w-2xl flex flex-col items-center h-full">
+        <div class="flex-1" />
 
-              <p class="welcome-text">
-                Combining quality, price, and friendly customer service, we offer one of
-                the best property improvement services in Melbourne. Read our
-                testimonials, and get in touch with us for a free quote today.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+        <h1 class="text-4xl md:text-6xl font-bold">
+          SURGERY, FOR <span class="text-primary">SPACES</span>.
+        </h1>
 
-    <div class="phone_body text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <a href="tel:%200422%20782%20198">
-              <i>
-                <img class="static" src="/images/social/phone.png" />
-                <img class="rollover" src="/images/social/phone-hover.png" />
-              </i>
-              0422 782 198
-            </a>
-          </div>
-        </div>
+        <p class="text-lg py-8 flex-1">
+          Combining quality, price, and friendly customer service, we offer one of the
+          best property improvement services in Melbourne. Read our testimonials, and get
+          in touch with us for a free quote today.
+        </p>
+
+        <a href="tel:0422782198" class="group inline-flex items-center gap-3">
+          <img
+            src="/images/social/phone.png"
+            class="size-7 md:size-8 grayscale group-hover:grayscale-0 transition-all duration-300"
+          />
+          <span
+            class="text-3xl md:text-4xl group-hover:text-primary transition-all duration-300"
+          >
+            0422 782 198
+          </span>
+        </a>
       </div>
     </div>
   </section>
 </template>
-
-<style scoped>
-section {
-  width: 100%;
-  height: auto;
-}
-
-.welcome {
-  display: table;
-  width: 100%;
-  height: auto;
-  padding: 100px 0;
-  text-align: center;
-  color: #fff;
-  background: url(/images/welcome-section-bg.jpg) top center no-repeat #000;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  background-size: cover;
-  -o-background-size: cover;
-}
-.welcome-body {
-  display: table-cell;
-  vertical-align: middle;
-}
-.welcome-body .brand-heading {
-  font-size: 42px;
-  margin-bottom: 25px;
-}
-.welcome-body .welcome-text {
-  font-size: 18px;
-}
-
-.phone_body {
-  margin-top: -80px;
-}
-.phone_body a {
-  color: #ffffff;
-  font-size: 40px;
-  vertical-align: top;
-  line-height: 33px;
-  -webkit-transition: all ease 0.5s;
-  -moz-transition: all ease 0.5s;
-  -ms-transition: all ease 0.5s;
-  -o-transition: all ease 0.5s;
-  transition: all ease 0.5s;
-}
-.phone_body a:hover {
-  color: #eb5e54;
-}
-.phone_body a:hover i {
-  color: #eb5e54;
-}
-.phone_body a img {
-  width: 33px;
-  margin-right: 7px;
-}
-.phone_body a img.static {
-  opacity: 1;
-  position: absolute;
-  -webkit-transition: all ease 0.3s;
-  -moz-transition: all ease 0.3s;
-  -ms-transition: all ease 0.3s;
-  -o-transition: all ease 0.3s;
-  transition: all ease 0.3s;
-}
-.phone_body a img.rollover {
-  opacity: 0;
-  margin-top: -8px;
-  -webkit-transition: all ease 0.3s;
-  -moz-transition: all ease 0.3s;
-  -ms-transition: all ease 0.3s;
-  -o-transition: all ease 0.3s;
-  transition: all ease 0.3s;
-}
-.phone_body a:hover img.static {
-  opacity: 0;
-}
-.phone_body a:hover img.rollover {
-  opacity: 1;
-}
-
-@media (max-width: 767px) {
-  .phone_body a {
-    font-size: 30px;
-    line-height: 27px;
-  }
-  .phone_body a img {
-    width: 25px !important;
-  }
-}
-
-@media (min-width: 767px) {
-  section,
-  .welcome {
-    height: 100%;
-    padding: 0;
-  }
-  .welcome-body .brand-heading {
-    margin: 0 auto 3px;
-    font-size: 64px;
-    font-weight: 700;
-    text-align: center;
-    text-transform: uppercase;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.18);
-    letter-spacing: -0.5px;
-  }
-  .welcome-body .welcome-text {
-    font-size: 18px;
-    margin-bottom: 50px;
-    text-align: center;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.28);
-  }
-}
-</style>
