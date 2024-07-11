c3ed80160de63a1ad4d0c54e9f22cf640b914725
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-11 12:57:33 +0200

    Migrate page scroller component

--- src/components/PageScroller.vue
+++ src/components/PageScroller.vue
@@ -1,40 +1,14 @@
 <script setup lang="ts">
+import { useScroll } from "../composables/useScroll";
 import Icon from "./Icon.vue";
+
+const { wasScrolled } = useScroll(200);
 </script>
 
 <template>
-  <div class="scroll-to-top affix" data-spy="affix" data-offset-top="200">
-    <a href="#page-top">
-      <Icon name="arrow-up" />
+  <div v-show="wasScrolled" class="fixed bottom-4 right-4">
+    <a href="#page-top" class="btn btn-square">
+      <Icon name="arrow-up" style="width: 18px; height: 18px" />
     </a>
   </div>
 </template>
-
-<style scoped>
-.scroll-to-top {
-  background: #333;
-  position: fixed;
-  bottom: 15px;
-  right: 15px;
-  text-align: center;
-  border-radius: 4px;
-  z-index: 9999;
-  opacity: 0;
-  filter: alpha(opacity=0);
-  -webkit-transition: opacity 0.5s;
-  transition: opacity 0.5s;
-  opacity: 0.5;
-  filter: alpha(opacity=50);
-}
-.scroll-to-top:hover {
-  opacity: 1;
-  filter: alpha(opacity=100);
-}
-.scroll-to-top a {
-  color: #fff;
-  display: block;
-  line-height: 40px;
-  width: 40px;
-  height: 40px;
-}
-</style>
