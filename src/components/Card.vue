7037ebee1821e12a92d4ac75627d041ffcd63133
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-12 23:26:54 +0200

    Add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  overlaySrc?: string;
+  centered?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="card overflow-hidden shadow-lg">
+    <!-- overlay -->
+    <figure v-if="overlaySrc">
+      <img :src="overlaySrc" :alt="title" loading="lazy" />
+    </figure>
+
+    <div class="card-body p-8">
+      <!-- title -->
+      <h2 v-if="title" class="card-title uppercase">
+        {{ title }}
+      </h2>
+
+      <!-- content -->
+      <slot />
+    </div>
+  </div>
+</template>
