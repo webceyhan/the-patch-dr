6b1ae432a805965cce5d92b9fa89881c27401c94
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-13 16:47:23 +0200

    Refactor call-action button

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  href?: string;
+  large?: boolean;
+}>();
+</script>
+
+<template>
+  <a
+    :class="[
+      'btn gap-4',
+      {
+        'btn-block h-16 text-3xl tracking-widest rounded-full': large,
+      },
+    ]"
+    :href
+  >
+    <slot>{{ label }}</slot>
+  </a>
+</template>
