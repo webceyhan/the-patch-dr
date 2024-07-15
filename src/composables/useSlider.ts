c4c9c526b88fa06ef04bf0b1e75541e8f934f3d9
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-15 09:12:00 +0200

    Add progress animation to testimonial slider

--- src/composables/useSlider.ts
+++ src/composables/useSlider.ts
@@ -2,11 +2,15 @@ import { computed, onMounted, onUnmounted, ref } from 'vue';
 
 export function useSlider<T>(items: T[], interval: number = 5000) {
     let intervalId: number;
-
+    const timeout = 10;
+    const secondCounter = ref(0);
+    const progress = computed(
+        () => (secondCounter.value / (interval / timeout)) * 100
+    );
+ 
     const isPlaying = ref(true);
     const index = ref(0);
     const item = computed(() => items[index.value]);
-
     const pause = () => (isPlaying.value = false);
     const resume = () => (isPlaying.value = true);
     const next = (i?: number) => {
@@ -14,12 +18,27 @@ export function useSlider<T>(items: T[], interval: number = 5000) {
     };
 
     onMounted(() => {
-        setInterval(() => isPlaying.value && next(), interval);
+        // tick every second
+        setInterval(() => {
+            // skip if not playing
+            if (!isPlaying.value) {
+                return;
+            }
+
+            // reset counter if interval passed
+            if (secondCounter.value === interval / timeout) {
+                secondCounter.value = 0;
+                return next();
+            }
+
+            // increment counter
+            secondCounter.value++;
+        }, timeout);
     });
 
     onUnmounted(() => {
         clearInterval(intervalId);
     });
 
-    return { index, item, isPlaying, pause, resume, next };
+    return { index, item, isPlaying, progress, pause, resume, next };
 }
