b37ded43a12b845ebbc4bf9205d694f27b854d28
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-21 17:43:07 +0200

    Fix dynamic asset url prefix

--- src/composables/useAsset.ts
+++ src/composables/useAsset.ts
@@ -1,5 +1,10 @@
-const BASE_URL = import.meta.url;
+const BASE_URL = import.meta.env.BASE_URL ?? '/';
 
 export function useAsset(uri: string): string {
-    return new URL(uri, BASE_URL).href;
+    // If the URI starts with a slash, replace it with the base URL
+    if (uri?.startsWith('/')) {
+        return BASE_URL + uri.slice(1);
+    }
+
+    return uri;
 }
