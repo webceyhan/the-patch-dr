10c6159ccdc8430c1c4b082d5868a0a2b5671f7c
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-22 13:44:17 +0200

    Fix tiktok social link icon

--- src/sections/ContactSection.vue
+++ src/sections/ContactSection.vue
@@ -34,7 +34,7 @@ import Link from "../components/Link.vue";
       <!-- social links -->
       <nav class="grid grid-flow-col gap-4">
         <Link v-for="link in SOCIAL_LINKS" :href="link.href!" target="_blank" greyed>
-          <Icon :name="link.icon!" class="size-16 rounded-full ring-4 ring-base-content" />
+          <Icon :name="link.icon!" class="size-16 rounded-full ring-4" />
         </Link>
       </nav>
 
