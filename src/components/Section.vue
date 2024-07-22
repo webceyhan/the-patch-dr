cfeaf91ea3ba46b2e07eae76eb0ac2a885528486
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-23 00:07:34 +0200

    Add dark theming to section component

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -8,6 +8,7 @@ import { useLazyBackground } from "../composables/useLazyBackground";
 interface Props {
   overlaySrc?: string;
   centered?: boolean;
+  dark?: boolean;
 }
 
 const { overlaySrc } = defineProps<Props>();
@@ -20,18 +21,18 @@ overlaySrc && useLazyBackground(section, overlaySrc);
 <template>
   <section
     ref="section"
+    :data-theme="dark || overlaySrc ? 'dark' : 'light'"
     :class="[
       'hero min-h-screen px-4 py-10 md:py-20 spy',
       {
-        'bg-base-100 text-base-content': !overlaySrc, // light (default)
-        'bg-base-content text-base-100 bg-cover bg-center bg-fixed bg-no-repeat': overlaySrc, // dark
+        'bg-cover bg-center bg-fixed bg-no-repeat': overlaySrc,
       },
     ]"
   >
     <!-- overlay -->
     <slot name="overlay" />
 
-    <div :class="['hero-content text-lg', { 'text-center': centered }]">
+    <div :class="['hero-content text-lg md:text-xl font-semibold', { 'text-center': centered }]">
       <div :class="['flex flex-col gap-10', { 'items-center': centered }]">
         <!-- content -->
         <slot />
