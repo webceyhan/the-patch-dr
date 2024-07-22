3a15a460d26a86183b9bac1797d8935cdc2cb772
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-07-22 12:02:11 +0200

    Add vsharp image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vsharp from 'vite-plugin-vsharp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './', // fix for github-pages
-    plugins: [vue()],
+    plugins: [vue(), vsharp()],
 });
